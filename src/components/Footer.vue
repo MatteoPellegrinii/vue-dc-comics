11f988f6f3ce06fbdcba3098526f9f7230b86927
MatteoPellegrinii <pellegrini.matteo00@gmail.com>
2022-06-08 17:49:39 +0200

    push

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,12 @@
 <template>
-<div></div>
+<div>
+    <div class="container">
+        <div class="list"></div>
+        <div class="logo"></div>
+    </div>
+    <div class="lastfoot"></div>
+</div>
+
 </template>
 
 <script>
@@ -7,3 +14,31 @@ export default {
   name: 'FooterProject',
 }
 </script>
+
+<style scoped lang="scss">
+.container {
+    background-image: url("../assets/img/footer-bg.jpg");
+    width: 100%;
+    height: 400px;
+    background-repeat: no-repeat;
+    background-size: cover;
+    display: flex;
+}
+.list{
+    width: 40%;
+}
+.logo{
+    width: 60%;
+    background-image: url(../assets/img/dc-logo-bg.png);
+    background-repeat: no-repeat;
+    background-position: center;
+    filter: opacity(0.5);
+
+}
+.lastfoot{
+    width: 100%;
+    background-color: gray;
+    height: 100px;
+}
+
+</style>
